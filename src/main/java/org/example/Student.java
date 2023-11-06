b98bf0a91c77e997654eea656ec67c4a70e7870f
PrisCristi <santos.priscila85@gmail.com>
2023-11-06 15:37:08 +0100

    Classes and adributtes created

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Student {
+    private String studentName;
+    private String studentId;
+    private String studentCourse;
+
+    public Student(String studentName, String studentId, String studentCourse) {
+        this.studentName = studentName;
+        this.studentId = studentId;
+        this.studentCourse = studentCourse;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentCourse() {
+        return studentCourse;
+    }
+
+    public void setStudentCourse(String studentCourse) {
+        this.studentCourse = studentCourse;
+    }
+}
